751d80511c8bfabcad1c75b67714e1181a579b56
unknown <lordwuiserous@gmail.com>
2024-12-22 17:05:55 +0530

    changed the design and name

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <TodoContextProvider>
       <div className='flex flex-col justify-start w-[600px] h-fit gap-2 rounded'>
-      <h1 className='w-full rounded p-2 bg-[#388E3C] bg-opacity-45'>Your Todo List</h1>
+      <h1 className='w-full headline rounded text-white p-2 bg-[#388E3C] bg-opacity-45'>The Forever List</h1>
       <AddTodo/>
       <ShowTodo />
       </div>
